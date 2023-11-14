114f41a76b65578c395ffe57d1ae684ffafb35c3
Ayush-gur <84288155+Ayush-gur@users.noreply.github.com>
2023-11-13 19:46:36 -0500

    the whole ah game
    sure

---
+++ VRSHOOT(2 weeks)/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAI : MonoBehaviour, ITakeDamage
+{
+   
+
+    [SerializeField] private float startingHealth;
+    [SerializeField] private int minShotsToTake;
+    [SerializeField] private int maxShotsToTake;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float damage; 
+
+    private bool isShooting;
+    private int currentShotsTaken;
+    private int currentMaxShotsToTake;
+    private NavMeshAgent agent;
+    private Player player;
+    private Transform occupiedCoverSpot;
+    private Animator animator;
+
+    private float _health;
+    public float health
+    {
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _health = Mathf.Clamp(value, 0, startingHealth);
+        }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        _health = startingHealth;
+    }
+
+    public void Init(Player player, Transform coverSpot)
+    {
+        this.player = player;
+    }
+
+
+    private void Update()
+    {
+       
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        Vector3 direction = player.GetHeadPosition() - transform.position;
+        direction.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        transform.rotation = rotation;
+    } 
+
+    public void TakeDamage(Weapon weapon, Projectile projectile, Vector3 contactPoint)
+    {
+        health -= weapon.GetDamage();
+        if (health <= 0)
+            Destroy(gameObject);
+       
+    }
+
+
+}
